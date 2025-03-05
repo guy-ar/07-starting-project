6084914369fc89c1486472d0bbbbf8f202a359d9
guy arbily <guy.arbilly@gmail.com>
2025-03-05 20:55:52 +0200

    chapter 9 section 182 - having the service using element injector for componenet and child components

--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
 
-@Injectable({
-  providedIn: "root"
-})
+// @Injectable({
+//   providedIn: "root"
+// })
 export class TaskService {
     private tasks = signal<Task[]>([]);
     allTasks = this.tasks.asReadonly(); // expose the signal as a readonly signal
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TaskService } from './task.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
-  imports: [NewTaskComponent, TasksListComponent],
+  imports: [NewTaskComponent, TasksListComponent], // these "chilkd components" have access to the same instance of the service
+  providers: [TaskService], // inseted injectable by root - this is element injector
 })
 export class TasksComponent {}
